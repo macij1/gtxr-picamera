2f9d4482869c9e602b998205af7d02e70b68d50f
macij1 <juanrmaci@gmail.com>
2025-07-11 23:07:52 -0700

    modified video_processing, wrote take selfie bash script that is reilient to existent video recording

--- fmpeg_processing.sh
+++ fmpeg_processing.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Define video directories here:
-DIRS=("")
+DIRS=("long_video")
 
 for DIR in "${DIRS[@]}"
 do
     echo "Processing $DIR"
-    
+
     cd "$DIR" || { echo "Directory $DIR not found"; continue; }
 
     ls video*.h264 | sort | sed "s/^/file '/;s/$/'/" > files.txt
 
-    ffmpeg -r 120 -f concat -safe 0 -i files.txt -c:v libx264 -preset slow -crf 18 "$DIR"_slowmo.mp4
+    ffmpeg -r 110 -f concat -safe 0 -i files.txt -c:v libx264 -preset slow -crf 18 "$DIR"_slowmo.mp4
 
     ffmpeg -i "$DIR"_slowmo.mp4 -vf "setpts=(1/4)*PTS" -an "$DIR"_realtime.mp4
 
---
+++ take_selfie.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p "/home/gtxr/gtxr-picamera/logs"
+exec | "/home/gtxr/gtxr-picamera/logs/picture.txt" 2>&1
+set -x
+
+# Absolute path to your project directory
+PROJECT_DIR="/home/gtxr/gtxr-picamera"
+date=$(date +%Y-%m-%d)
+TOP_LOG="/home/gtxr/gtxr-picamera/logs/main_log_${date}.txt"
+
+# Navigate to the project directory
+cd "$PROJECT_DIR"
+
+# Activate the virtual environment
+source "picamvenv/bin/activate"
+
+# Check that the background process is running
+echo "$(date): Killing camera task manager" | "$TOP_LOG"
+sudo pkill -f "task"
+
+echo "$(date): Running selfie script" | "$TOP_LOG"
+sudo /home/gtxr/gtxr-picamera/picamvenv/bin/python -u take_selfie.py
